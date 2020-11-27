711c9e48568aca91b8ad8592c7cbad43c0dcb354
PB <lindynetech@gmail.com>
2020-11-27 15:15:15 -0500

    Run next build

--- build-test.sh
+++ build-test.sh
@@ -2,16 +2,18 @@
 docker_username=$1
 set -xe
 
-curl -sL https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 -o /usr/local/bin/kind
-chmod 755 /usr/local/bin//kind
+# curl -sL https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 -o /usr/local/bin/kind
+# chmod 755 /usr/local/bin//kind
 
-curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
-chmod 755 /usr/local/bin//kubectl
+# curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+# chmod 755 /usr/local/bin//kubectl
 
-curl -LO https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz
-tar -xzf helm-v3.1.2-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/
-rm -rf helm-v3.1.2-linux-amd64.tar.gz
+# curl -LO https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz
+# tar -xzf helm-v3.1.2-linux-amd64.tar.gz
+# mv linux-amd64/helm /usr/local/bin/
+# rm -rf helm-v3.1.2-linux-amd64.tar.gz
+
+# kind, kubectl, helm all included in ubuntu-latest (18.04)
 
 kind version
 kubectl version --client=true
